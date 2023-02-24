ea9e231f69792fb0676df22f29d19c87c7b020ea
ank501 <ankitsyadav246@gmail.com>
2023-02-25 01:51:51 +0530

    admin panel done

--- medpharma/src/Context/AuthContextProvider.jsx
+++ medpharma/src/Context/AuthContextProvider.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { createContext ,useState} from "react";
+import {Navigate} from "react"
+export const AuthContext = createContext()
 
-const AuthContextProvider = () => {
-  return (
-    <div>AuthContextProvider</div>
-  )
+function AuthContextProvider({children}) {
+    const [authState ,setAuthstate] = useState({
+        isAuth :false
+        ,token : null
+    })
+   // console.log(authState)
+    const loginUser =(tok) =>{
+     if(tok){
+        setAuthstate({isAuth:true ,token:tok});
+       
+     }
+
+    }
+    const logoutUser =() =>{
+        setAuthstate(authState.isAuth=false,authState.token=null);
+      <Navigate to="/" />
+    }
+
+    return <AuthContext.Provider value={{ authState, loginUser,logoutUser }} >{children}</AuthContext.Provider>
 }
 
-export default AuthContextProvider
+export default AuthContextProvider;
